b9de7f57f02dfba2c201a4512d9a281cbe0a906c
cano <contact.sasasaky@gmail.com>
2020-03-13 17:34:31 +0900

    build

--- dist/typescript.js
+++ dist/typescript.js
@@ -1,1 +1,1 @@
-module.exports={extends:["./no-ts"],parser:"@typescript-eslint/parser",plugins:["@typescript-eslint"],rules:{"@typescript-eslint/adjacent-overload-signatures":"error","@typescript-eslint/array-type":["warn",{default:"array-simple"}]}};
+module.exports={extends:["./base"],parser:"@typescript-eslint/parser",plugins:["@typescript-eslint"],rules:{"@typescript-eslint/adjacent-overload-signatures":"error","@typescript-eslint/array-type":["warn",{default:"array-simple"}]}};
