27461ca94b85ae74c782be985f057bb0cf52aece
Cano <contact.sasasaky@gmail.com>
2020-06-17 21:36:58 +0900

    resolve path

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const Terser = require("terser");
 
-const src = "src";
+const src = "rules";
 
 const build = (outputPath, filePath) => {
   const file = fs.readFileSync(filePath);
